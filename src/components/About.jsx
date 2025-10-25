e07124e233a90b82e63bd8e1eb07f7ff6f3086e5
menna <nonaelbadry21@gmail.com>
2025-10-25 17:33:45 +0300

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,7 @@ export default function About(){
     return(
         <section className="@container flex flex-col mx-auto px-20 py-10 max-sm:px-10  max-sm:text-sm">
             <p className="  text-gray-200 hover:text-gray-50 text-2xl underline cardo-regular-italic">About me :</p>
-            <p className="  text-gray-200 hover:text-gray-50 cardo-regular-italic text-xl indent-18 leading-[3rem] max-md:text-lg max-md:leading-5 ">Frontend Developer skilled in React, TailwindCSS v4, and Bootstrap5, with growing experience in PHP, MySQL, …
-         Passionate about building responsive and user-friendly web applications.</p>
+            <p className="  text-gray-200 hover:text-gray-50 cardo-regular-italic text-xl indent-18 leading-[3rem] max-md:text-lg max-md:leading-5 ">Frontend Developer skilled in React, TailwindCSS v4, and Bootstrap5, with growing experience in PHP, MySQL, … Passionate about building responsive and user-friendly web applications.</p>
             
         </section>
     )
