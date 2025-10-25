25db17348ceeb1aafe257f3913c428de15bfd96d
menna <nonaelbadry21@gmail.com>
2025-10-25 14:38:21 +0300

    initial commit

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//     theme: {
-//         extend: {
-//             fontFamily: {
-//                 cardo: ["Cardo", "serif"],
-//             },
-//         },
-//     },
-// };
